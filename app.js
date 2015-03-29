47c63be7a37ee437b4ab980d6fa475f2be7223e4
steelydylan <ess_president@me.com>
2015-03-29 19:30:32 +0900

    mongoDB導入

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var cheerio = require("cheerio");
 var request = require("request");
-var items = [];
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+mongoose.connect(process.env.MONGOHQ_URL || "mongodb://localhost:27017/signage");
+mongoose.model('entry',new Schema({
+  html:String,
+  bid:Number,
+  eid:Number
+}));
+var entries = mongoose.model('entry');
 app.use(require('express').static(__dirname+'/public'));
 app.engine('html', require('ejs').renderFile);
 server.listen(process.env.PORT || 3000);
@@ -13,7 +22,7 @@ app.get('/', function (req, res) {
   res.render("index.html");
 });
 app.get('/app',function(req,res){
-	res.render("app.html",{items:items});
+	res.render("app.html");
 });
 
 io.on('connection', function (socket) {
@@ -28,18 +37,24 @@ io.on('connection', function (socket) {
 
   //サイネージに流すエントリーを登録
   socket.on('setEntries',function(msg){
-  	items = [];
-  	msg.entries.forEach(function(item){
-  		request({uri: msg.url+"/bid/"+item.bid+"/eid/"+item.eid},function(error,response,body){
-  			var html = util.getEntryHtml(msg,body);
-  			items.push({eid:item.eid,bid:item.bid,html:html});
-  		});
-  	});
+    entries.remove({},function(){
+    	msg.entries.forEach(function(item){
+    		request({uri: msg.url+"/bid/"+item.bid+"/eid/"+item.eid},function(error,response,body){
+          var entry = new entries();
+    			entry.html = util.getEntryHtml(msg,body);
+          entry.bid = item.bid;
+          entry.eid = item.eid;
+    			entry.save();
+    		});
+    	});
+    });
   });
 
   //サイネージに流すエントリーを送信
   socket.on('getEntries',function(msg){
-  	socket.emit('getEntries',items);
+    entries.find({},function(err,items){
+  	 socket.emit('getEntries',items);
+    });
   });
 
   //割り込みようのサイネージを取得し送信
